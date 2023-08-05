a7680512124d6d0abc123b51716988398b6404bb
Tim <corsairtim89@gmail.com>
2023-08-05 18:34:58 +0300

    refactor singular lr into class

--- src/linearregression/main.py
+++ src/linearregression/main.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn_lr import linear_regression
-from singular_lr_scratch import fit
+from singular_lr_scratch import SingularLinearRegression
 
 
 PREDICTOR = np.array([5, 15, 25, 35, 45, 55])
@@ -9,14 +9,13 @@ SK_PREDICTOR = PREDICTOR.reshape(-1, 1)
 TARGET = np.array([30, 21, 27, 39, 40, 37])
 LEARN_RATE = 0.0008
 EPOCHS = 10000
-slope = 0
-intercept = 0
 
-slope, intercept = fit(EPOCHS, slope, intercept, PREDICTOR, TARGET, LEARN_RATE)
-model = linear_regression(SK_PREDICTOR, TARGET)
+model = SingularLinearRegression(PREDICTOR, TARGET, LEARN_RATE, EPOCHS)
+model.fit()
+sklearn_model = linear_regression(SK_PREDICTOR, TARGET)
 
 fig, ax = plt.subplots()
 ax.scatter(PREDICTOR, TARGET, color='g')
-ax.plot([slope*x + intercept for x in range(PREDICTOR[0], PREDICTOR[-1])], color='r')
-ax.plot(SK_PREDICTOR, model.predict(SK_PREDICTOR), color='b')
+ax.plot(model.plot_line(), color='r')
+ax.plot(SK_PREDICTOR, sklearn_model.predict(SK_PREDICTOR), color='b')
 plt.show()
--- src/linearregression/singular_lr_scratch.py
+++ src/linearregression/singular_lr_scratch.py
@@ -8,48 +8,59 @@ https://vitalflux.com/interpreting-f-statistics-in-linear-regression-formula-exa
 """
 
 
+from dataclasses import dataclass
 import numpy as np
 import util as u
 
 
-def gradient_descent(slope, intercept, predictor: np.ndarray, target: np.ndarray, learn_rate):
-    """Attempts to minimize loss by way of gradient descent (loss function in this case is mean squared error(MSE)).
+@dataclass
+class SingularLinearRegression:
+    predictor: np.ndarray
+    target: np.ndarray
+    learn_rate: float
+    epochs: int
+    slope: float = 0
+    intercept: float = 0
 
-    Returns
-    -------
-        Parameters (slope and intercept) adjusted by negative gradient times the learning rate
-    """
-    slope_pd = 0
-    intercept_pd = 0
-    n = len(predictor)
+    def fit(self):
+        for i in range(self.epochs):
+            if (i % 1000 == 0):
+                print(f'Epoch: {i}, current slope: {self.slope}, current intercept: {self.intercept}')
+            self.__gradient_descent()
 
-    for i in range(n):
-        # calculate partial derivatives of MSE with respect to slope and intercept
-        slope_pd += -(2/n) * predictor[i] * (target[i] - (slope*predictor[i] + intercept))
-        intercept_pd += -(2/n) * (target[i] - (slope*predictor[i] + intercept))
+        print(f'The p value is {self.__calculate_p()}')
 
-    slope -= slope_pd*learn_rate
-    intercept -= intercept_pd*learn_rate
+    def plot_line(self):
+        return [self.slope*x + self.intercept for x in range(self.predictor[0], self.predictor[-1])]
 
-    return slope, intercept
+    def __gradient_descent(self):
+        """Attempts to minimize loss by way of gradient descent (loss function in this case is mean squared error(MSE)).
 
+        Returns
+        -------
+            Parameters (slope and intercept) adjusted by negative gradient times the learning rate
+        """
+        slope_pd = 0
+        intercept_pd = 0
+        n = len(self.predictor)
 
-def fit(epochs, m, b, x, y, lr):
-    for i in range(epochs):
-        if (i % 1000 == 0):
-            print(f'Epoch: {i}, current slope: {m}, current intercept: {b}')
-        m, b = gradient_descent(m, b, x, y, lr)
+        for i in range(n):
+            x = self.predictor[i]
+            y = self.target[i]
+            # calculate partial derivatives of MSE with respect to slope and intercept
+            slope_pd += -(2/n) * x * (y - (self.slope*x + self.intercept))
+            intercept_pd += -(2/n) * (y - (self.slope*x + self.intercept))
 
-    # Compute the p-value
-    se_mean = u.squared_error_total(y)
-    se_line = u.squared_error_regression(x, y, m, b)
-    # Degrees of freedom in the denominator,
-    # number of observations minus 2 extra parameters in the model (slope and intercept)
-    d_dof = len(y) - 2
-    var_explained = se_mean-se_line
-    var_ratio = var_explained / (se_line/d_dof)
-    # Note: degrees of freedom in the numerator are equal to 1 in the case of 2d linear model
-    p_val = u.f_test(var_ratio, 1, d_dof)
-    print(f'The p value is {p_val}')
+        self.slope -= slope_pd*self.learn_rate
+        self.intercept -= intercept_pd*self.learn_rate
 
-    return m, b
+    def __calculate_p(self):
+        se_mean = u.squared_error_total(self.target)
+        se_line = u.squared_error_regression(self.predictor, self.target, self.slope, self.intercept)
+        # Degrees of freedom in the denominator,
+        # number of observations minus 2 extra parameters in the model (slope and intercept)
+        d_dof = len(self.target) - 2
+        var_explained = se_mean-se_line
+        var_ratio = var_explained / (se_line/d_dof)
+        # Note: degrees of freedom in the numerator are equal to 1 in the case of 2d linear model
+        return u.f_test(var_ratio, 1, d_dof)
