b120dc4f0569b0d17b5f00f3df8913c2bb6e4521
Tim <corsairtim89@gmail.com>
2023-08-07 19:23:32 +0300

    update env

--- src/linearregression/simple_lr_sklearn.py
+++ src/linearregression/simple_lr_sklearn.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 def linear_regression(x: ndarray, y: ndarray) -> LinearRegression:
+    # Note: sklearn uses the least squares method for optimization (solve Ax=b)
     model = LinearRegression(fit_intercept=True, copy_X=False, n_jobs=None).fit(x, y)
     logger.info(f'Slope: {model.coef_}, Intercept: {model.intercept_}')
     return model
