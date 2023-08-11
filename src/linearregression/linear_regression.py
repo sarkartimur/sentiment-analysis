f601cfae5064e009b48042ce5ecd5a9d3a8b2248
Tim <corsairtim89@gmail.com>
2023-08-11 20:06:21 +0300

    standardize data, remove intercept

--- src/linearregression/linear_regression.py
+++ src/linearregression/linear_regression.py
@@ -21,46 +21,42 @@ class GradLinearRegression:
     epochs: InitVar[int]
 
     slope: np.ndarray[int, np.dtype[np.float64]] = np.random.uniform(0, 1, (2))
-    intercept: float = np.random.uniform(0, 100)
 
     def __post_init__(self, learn_rate, epochs):
         self.__learn_rate = learn_rate
         self.__epochs = epochs
 
     def fit(self, predictor, target):
-        """Attempts to minimize loss by way of batch gradient descent (loss function in this case is mean squared error(MSE))."""
+        """Attempts to minimize loss by way of batch gradient descent (loss function in this case is square error)."""
         for i in range(self.__epochs):
-            if (i % 10000 == 0):
-                logger.debug(f'Epoch: {i}, current slope: {self.slope}, current intercept: {self.intercept}')
+            logger.debug(f'Epoch: {i}, current slope: {self.slope}')
             self.__gradient_descent(predictor, target)
         logger.info(f'The p value is {self.__calculate_p(predictor, target)}')
-        logger.info(f'Slope: {self.slope}, Intercept: {self.intercept}')
+        logger.info(f'Slope: {self.slope}')
         return self
 
     def plot_line(self, predictor):
-        return [self.slope*x + self.intercept for x in range(0, predictor[-1])]
+        return [self.slope*x for x in range(0, predictor[-1])]
 
     def __gradient_descent(self, predictor, target):
-        line = (self.slope*predictor).sum(1) + self.intercept
-        # calculate partial derivatives of MSE with respect to slope and intercept
+        line = (self.slope*predictor).sum(1)
+        # calculate partial derivatives of MSE with respect to each parameter (slope)
         slope_pd = (-2*predictor*(target.reshape(-1, 1) - line.reshape(-1, 1))).sum(0)
-        intercept_pd = (-2*(target - line)).sum()
 
         # adjust parameters by negative gradient times the learning rate
         self.slope -= slope_pd*self.__learn_rate
-        self.intercept -= intercept_pd*self.__learn_rate
 
     def __calculate_p(self, predictor, target):
         mean = target.mean()
         sst = ((target - mean)**2).sum()
-        plane = (self.slope*predictor).sum(1) + self.intercept
+        plane = (self.slope*predictor).sum(1)
         sse = ((target - plane)**2).sum()
         ssr = sst-sse
         # Degrees of freedom in the numerator,
         # number of coefficients in the model
         ssr_dof = len(self.slope)
         # Degrees of freedom in the denominator,
-        # number of observations minus extra parameters in the model (coefficients and intercept)
-        sse_dof = len(target) - (ssr_dof + 1)
+        # number of observations minus extra parameters in the model (coefficients)
+        sse_dof = len(target) - ssr_dof - 1
         f = (ssr/ssr_dof) / (sse/sse_dof)
         return 1 - scipy.stats.f.cdf(f, ssr_dof, sse_dof)
--- test/test_linear_regression.py
+++ test/test_linear_regression.py
@@ -1,20 +1,21 @@
+import time
 from src.linearregression.linear_regression import GradLinearRegression
 from sklearn.linear_model import LinearRegression
-from math import floor
-import time
 from matplotlib import pyplot as plt
 import numpy as np
 np.random.seed(777)
 
 
-PREDICTOR = np.random.randint(0, 100, size=(10, 2))
-TARGET = np.random.randint(0, 100, size=10)
-LEARN_RATE = 0.00001
-EPOCHS = 200000
+def data():
+    return standardize(np.random.normal(0, 100, size=(50, 2))), standardize(np.random.normal(0, 100, size=50))
+
+PREDICTOR, TARGET = data()
+LEARN_RATE = 0.01
+EPOCHS = 10
 
 model = GradLinearRegression(LEARN_RATE, EPOCHS)
 # Note: sklearn uses the least squares method for optimization (solve Ax=b)
-sk_model = LinearRegression(fit_intercept=True, copy_X=False, n_jobs=None)
+sk_model = LinearRegression(fit_intercept=False, copy_X=False, n_jobs=None)
 
 
 def test_linear_regression():
@@ -22,12 +23,11 @@ def test_linear_regression():
     sk_model.fit(PREDICTOR, TARGET)
     try:
         assert f'{sk_model.coef_[0]:.2f}' == f'{model.slope[0]:.2f}'
-        assert floor(sk_model.intercept_) == floor(model.intercept)
     except AssertionError as e:
-        x, y = np.linspace(0, 100, 10), np.linspace(0, 100, 10)
+        x, y = np.linspace(-2, 2, 10), np.linspace(-2, 2, 10)
         xs, ys = np.meshgrid(x, y)
-        zs = xs*model.slope[0] + ys*model.slope[1] + model.intercept
-        zs_sk = xs*sk_model.coef_[0] + ys*sk_model.coef_[1] + sk_model.intercept_
+        zs = xs*model.slope[0] + ys*model.slope[1]
+        zs_sk = xs*sk_model.coef_[0] + ys*sk_model.coef_[1]
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
@@ -41,3 +41,22 @@ def test_linear_regression():
         plt.savefig(f'linear_regression_{time.time()}_fail.png')
 
         raise e
+    
+
+def standardize(x: np.ndarray) -> np.ndarray:
+    """
+    Standardize data i.e. bring the mean to 0 (center data around 0),
+    bring the standard deviation to 1.
+
+    Parameters
+    ----------
+    x
+        Input array
+
+    Returns
+    -------
+        Standardized array
+    """    
+    mean = x.mean()
+    std = x.std()
+    return (x - mean)/std    
