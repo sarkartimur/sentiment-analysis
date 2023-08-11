fix p calculation

src/linearregression/linear_regression.py
@@ -52,16 +52,15 @@ class GradLinearRegression:
 
     def __calculate_p(self, predictor, target):
         mean = target.mean()
-        se_mean = ((target - mean)**2).sum()
+        sst = ((target - mean)**2).sum()
         plane = (self.slope*predictor).sum(1) + self.intercept
-        se_plane = ((target - plane)**2).sum()
-        # Degrees of freedom in the denominator,
-        # number of observations minus extra parameters in the model (slope and intercept)
-        d_dof = len(target) - (len(self.slope) + 1)
+        sse = ((target - plane)**2).sum()
+        ssr = sst-sse
         # Degrees of freedom in the numerator,
-        # number of params in the model (len(slope) + 1 for intercept) minus number of params without a model (just intercept)
-        n_dof = (len(self.slope) + 1) - 1
-        var_explained = se_mean-se_plane
-        var_ratio = (var_explained/n_dof) / (se_plane/d_dof)
-        # todo incorrect for some reason, figure it out
-        return 1 - scipy.stats.f.cdf(var_ratio, n_dof, d_dof)
+        # number of coefficients in the model
+        ssr_dof = len(self.slope)
+        # Degrees of freedom in the denominator,
+        # number of observations minus extra parameters in the model (coefficients and intercept)
+        sse_dof = len(target) - (ssr_dof + 1)
+        f = (ssr/ssr_dof) / (sse/sse_dof)
+        return 1 - scipy.stats.f.cdf(f, ssr_dof, sse_dof)