02a49c37c369f721cb6542b96b25d2aa52e97fdc
Tim <corsairtim89@gmail.com>
2025-09-02 00:30:02 +0300

    refactor svm_iris_oversampled

--- src/svm/svm_iris_oversampled.py
+++ src/svm/svm_iris_oversampled.py
@@ -10,7 +10,7 @@ from collections import Counter
 
 # Note: there are 50 samples of each of 3 classes, so 50 max
 SAMPLE_COUNT = 50
-REMOVE_COUNT = 40
+REMOVE_COUNT = 25
 REMOVE_CLASS = 'Iris-setosa'
 RANDOM_SEED = 42
 
@@ -20,14 +20,6 @@ iris = fetch_ucirepo(id=53)
 df: pd.DataFrame = iris.data.original
 df = df.groupby('class').sample(n=SAMPLE_COUNT, random_state=RANDOM_SEED)
 
-
-"""
-Remove some samples
-"""
-to_remove = df[df['class'] == REMOVE_CLASS]
-to_remove = to_remove.sample(n=REMOVE_COUNT, random_state=RANDOM_SEED)
-df = df.drop(to_remove.index)
-
 """
 Split data into predictors and target
 """
@@ -35,19 +27,29 @@ X = df.drop('class', axis=1).copy()
 y = df['class'].copy()
 
 """
-Oversample using smote
-"""
-print(f"Original class distribution: {Counter(y)}")
-smote = SMOTE(random_state=42)
-X, y = smote.fit_resample(X, y)
-print(f"Resampled class distribution: {Counter(y)}")
-
-"""
 Split data into train/test sets, standardize.
 """
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=RANDOM_SEED)
+
+"""
+Remove some samples
+"""
+print(f"Original class distribution: {Counter(y_train)}")
+to_remove = y_train[y_train == REMOVE_CLASS].index
+to_remove = to_remove.to_series().sample(n=REMOVE_COUNT, random_state=RANDOM_SEED).index
+X_train = X_train.drop(to_remove)
+y_train = y_train.drop(to_remove)
+print(f"Class distribution after removal: {Counter(y_train)}")
+
+
+"""
+Oversample using smote
+"""
 X_train = scale(X_train)
 X_test = scale(X_test)
+smote = SMOTE(random_state=RANDOM_SEED)
+X_train, y_train = smote.fit_resample(X_train, y_train)
+print(f"Oversampled class distribution: {Counter(y_train)}")
 
 """
 Build and train a support vector classifier
